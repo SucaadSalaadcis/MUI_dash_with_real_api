8622f9419cd6e7a4db82593200a126a251b154dc
Sucaad Salaad Osman <140339872+SucaadSalaadcis@users.noreply.github.com>
2024-10-28 16:11:51 +0300

    Update Add_Edit_View.jsx

--- src/pages/reusible/Add_Edit_View.jsx
+++ src/pages/reusible/Add_Edit_View.jsx
@@ -107,3 +107,103 @@ export function Edit({ EditParam }) {
         </label>
     )
 }
+
+
+
+
+///// short way 
+
+// import { Box, Button } from "@mui/material";
+// import { Link, useLocation } from "react-router-dom";
+
+// import VisibilityIcon from '@mui/icons-material/Visibility';
+// import EditIcon from '@mui/icons-material/Edit';
+
+
+// const pathMap = [
+//     { path: '/user_management/permission', endpoint: 'permission', viewEndpoint: 'permission_view' },
+//     { path: '/agents', endpoint: 'agent', viewEndpoint: 'agent_view' },
+//     { path: '/customers', endpoint: 'customer', viewEndpoint: 'customer_view' },
+//     { path: '/products', endpoint: 'product', viewEndpoint: 'product_view' },
+//     { path: '/user_management/roles', endpoint: 'role', viewEndpoint: 'role_view' },
+//     { path: '/user_management/users', endpoint: 'users', viewEndpoint: 'users_view' },
+//     { path: '/orders', endpoint: 'order', viewEndpoint: 'order_view' },
+// ];
+
+
+
+// // edit functionallity
+// export function Edit({ EditParam }) {
+
+//     const location = useLocation();
+
+//     const match = pathMap.find(item => item.path === location.pathname);
+//     // console.log(match)
+//     if (!match) return null;
+
+//     // Construct 
+//     const url = `/${match.endpoint}/${EditParam}`;
+
+//     return (
+//         <Link to={url}>
+//             <EditIcon style={{ color: 'blue' }} />
+//         </Link>
+//     )
+// }
+
+
+// // veiw functionallity
+// export function View({ veiwParam }) {
+
+//     const location = useLocation();
+
+//     const match = pathMap.find(item => item.path === location.pathname);
+
+//     if (!match) return null;
+
+//     const url = `/${match.viewEndpoint}/${veiwParam}`;
+
+//     return (
+//         <Link to={url}>
+//             <VisibilityIcon style={{ color: "green", marginLeft: '8px' }} />
+//         </Link>
+//     )
+// }
+
+
+// // add functionallity
+// export function Add() {
+
+//     const location = useLocation();
+
+//     const match = pathMap.find(item => item.path === location.pathname);
+//     // console.log(match)
+//     if (!match) return null;
+
+//     // Construct 
+//     const url = `/${match.endpoint}`;
+
+//     return (
+//         <Box
+//             sx={{
+//                 display: 'flex',
+//                 justifyContent: 'flex-end',  // Push to the right
+//                 width: '100%',
+//             }}>
+//             <Link to={url}>
+//                 <Button variant='contained' sx={{ backgroundColor: '#E53270' }}>
+//                     ADD
+//                 </Button>
+//             </Link>
+//         </Box>
+//     )
+// }
+
+
+
+
+
+
+
+
+
